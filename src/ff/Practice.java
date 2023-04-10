25416cf3aae9a32991f1b942a5463cd6c38986b8
JBEDU01 <JBEDU01@DESKTOP-HMF1SKG.mshome.net>
2023-04-10 14:35:57 +0900

    ff

---
+++ src/ff/Practice.java
@@ -0,0 +1,14 @@
+package ff;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j=0;
+		while(j<5) {
+			System.out.println("안녕");
+			j++;
+		}
+	}
+
+}
---
+++ src/ff/Practice2.java
@@ -0,0 +1,15 @@
+package ff;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<10;i++) {
+			if(i%2==1)
+				continue;
+			
+			System.out.println("나는 짝수이다!");
+		}
+	}
+
+}
